fc282a0a9bf89f6d6e5413e35b61d64baf66fd90
licebeam <dustinwalkerart@gmail.com>
2018-09-14 19:31:34 +0900

    added colors.js

---
+++ colors.js
@@ -0,0 +1,21 @@
+
+var colors = require('colors')
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const textArr = [
+  `Hack The Planet!`,
+  'Time.. Too.. Die..',
+  'Ah ah ah, you didn\'t say the magic work',
+  'Hold on to ya butts.',
+  'You gotta stop lettin ya momma dress you!',
+  'Hasta la vista baby',
+  'She\'s not my mother toddddd!',
+]
+const colorName = (text) => {
+  console.log(colors.rainbow(textArr[getRandomInt(textArr.length)]));
+}
+
+colorName(textArr); 

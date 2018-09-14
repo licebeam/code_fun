e75c89e2b74224d996f69b2dc624e0cd76c0688c
licebeam <dustinwalkerart@gmail.com>
2018-09-14 19:39:18 +0900

    updated quotes

--- colors.js
+++ colors.js
@@ -7,12 +7,15 @@ function getRandomInt(max) {
 
 const textArr = [
   `Hack The Planet!`,
-  'Time.. Too.. Die..',
+  'Time.. To.. Die..',
   'Ah ah ah, you didn\'t say the magic word',
   'Hold on to ya butts.',
   'You gotta stop lettin ya momma dress you!',
   'Hasta la vista baby',
   'She\'s not my mother toddddd!',
+  'Stay out!',
+  'You have been expunged.',
+  'Mess with the best die like the rest.',
 ]
 const colorName = (text) => {
   console.log(colors.rainbow(textArr[getRandomInt(textArr.length)]));

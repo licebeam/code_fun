333fe75aa45107640e25185f1535207165246bca
licebeam <dustinwalkerart@gmail.com>
2018-09-18 13:16:21 +0900

    added japanese quiz for bash profile

---
+++ japaneseQuiz.js
@@ -0,0 +1,36 @@
+
+var colors = require('colors')
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const japaneseQuestions = [
+  {
+    question: `行く | いく`,
+    answer: 'to go'
+  },
+  {
+    question: `見る | みる`,
+    answer: 'to see'
+  }
+]
+
+const colorName = (text) => {
+  let currentQuestion = japaneseQuestions[getRandomInt(japaneseQuestions.length)]
+  console.log(colors.yellow('What is the meaning of: ') + colors.green(currentQuestion.question));
+  rl.question(colors.yellow('What is the answer: '), (answer) => {
+    if (currentQuestion.answer === answer) {
+      console.log(colors.yellow('Good job, the correct answer was: ') + colors.rainbow(currentQuestion.answer));
+    } else console.log(colors.yellow("Sorry the correct asnwer is: ") + colors.red(currentQuestion.answer));
+    rl.close();
+  });
+}
+
+colorName(japaneseQuestions); 

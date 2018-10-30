4a4580145a9042ad3ae77be54bbb2d1a580391f8
licebeam <dustinwalkerart@gmail.com>
2018-10-30 11:47:21 +0900

    added new file cool.js

---
+++ cool.js
@@ -0,0 +1,13 @@
+const books = [{id: 1, text: 'I am a book', title: 'Salems Lot'}]
+
+const checkOut = () => {
+  //take book
+}
+
+const checkShelf = () => {
+  //look for books
+}
+
+const readBook = (book) => {
+  //reads a book
+}

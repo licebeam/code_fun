241d3b33bd1b3ed353189631a58ab8f1e401ef51
licebeam <dustinwalkerart@gmail.com>
2018-10-30 11:53:44 +0900

    added writer and readline

--- cool.js
+++ cool.js
@@ -1,13 +1,27 @@
-const books = [{id: 1, text: 'I am a book', title: 'Salems Lot'}]
+const readline = require('readline');
+const writer = (text) => {
+  console.log('\x1b[36m%s\x1b[0m', text)
+}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const books = [{ id: 1, text: 'I am a book', title: 'Salems Lot' }]
 
-const checkOut = () => {
+const checkOut = (book) => {
   //take book
 }
 
-const checkShelf = () => {
+const checkShelf = (books) => {
   //look for books
+  writer('You look deeply at the books');
+  rl.question('What book do you want to check out?', (answer) => {
+    console.log(`${answer}! Nice book choice`);
+  });
 }
 
 const readBook = (book) => {
   //reads a book
 }
+
+checkShelf(books);
